3eb8cc367bbb2bef7a0d42a714a1306655308ee8
emmettburgess <47261227+emmettburgess@users.noreply.github.com>
2019-05-23 16:02:52 -0600

    app updated

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import FriendList from "./FriendList";
+import AddForm from "./AddForm";
+import UpdateForm from "./UpdateForm";
+
+import "./App.css";
+
+import axios from "axios";
+
+import { Route, NavLink } from "react-router-dom";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      activeFriend: null,
+
+      postSuccessMessage: "",
+      postError: "",
+      putSuccessMessage: "",
+      putError: "",
+      deleteSuccessMessage: "",
+      deleteError: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => console.log(err));
+  }
+
+  postFriend = newFriend => {
+    axios
+      .post("http://localhost:5000/friends", newFriend)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          friends: res.data,
+          postSuccessMessage: res.data.successMessage
+        });
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
+  updateFriend = updatedFriend => {
+    axios
+      .put(`http://localhost:5000/friends/${updatedFriend.id}`, updatedFriend)
+      .then(res => {
+        console.log(res);
+        this.setState({ friends: res.data });
+        this.props.history.push("/");
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  };
+
+  setActiveFriend = friend => {
+    this.setState({ activeFriend: friend });
+  };
+
+  render() {
+    return (
+      <div className='page-style'>
+        <nav className="nav-bar">
+          <NavLink to="/"> <div className='nav-link-style'>HOME</div> </NavLink>
+          <NavLink to="/add"> <div className='nav-link-style'>ADD</div> </NavLink>
+          <NavLink to="/update"> <div className='nav-link-style'>UPDATE</div> </NavLink>
+        </nav>
+
+        <div className="App">
+          <Route
+            path="/"
+            render={props => (
+              <FriendList
+                friends={this.state.friends}
+                setActiveFriend={this.setActiveFriend}
+                {...props}
+              />
+            )}
+          />
+
+          <Route
+            path="/add"
+            render={props => (
+              <AddForm
+                friends={this.state.friends}
+                postFriend={this.postFriend}
+                postSuccessMessage={this.state.postSuccessMessage}
+                postErrorMessage={this.state.postError}
+                {...props}
+              />
+            )}
+          />
+
+          <Route
+            path="/update"
+            render={props => (
+              <UpdateForm 
+              activeFriend={this.state.activeFriend} 
+              updateFriend={this.updateFriend}
+              {...props}
+              />
+            )}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
